b115192c66b944bbd221cc4c49c2919d6191ada5
Yaroslav.Dusaniuk <yaroslav_dusaniuk@epam.com>
2019-10-03 21:57:23 +0300

    chore(lint): add command for linting

--- src/main.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from 'chai';
-import * as faker from 'faker';
-import { IncomingMessage } from 'telegraf/typings/telegram-types';
-
-import { Mention } from './models';
-
-describe('main', () => {});
