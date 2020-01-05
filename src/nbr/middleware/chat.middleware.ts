e312e9c64f76c56e4adc7c3a4be7253046548657
Yaroslav.Dusaniuk <yaroslav.dusaniuk@gmail.com>
2020-01-05 15:10:56 +0200

    fix(moddleware): add fix for isCommand check (for cases when there's no message)

--- src/nbr/middleware/chat.middleware.ts
+++ src/nbr/middleware/chat.middleware.ts
@@ -3,7 +3,7 @@ import { Middleware } from 'telegraf';
 import { AppContext } from '../../shared/models/appContext';
 
 const filterNonPrivateChats = async (ctx: AppContext, next: () => any): Promise<{}> => {
-  const isCommand: boolean = (ctx.message.entities || []).some(entity => entity.type === 'bot_command');
+  const isCommand: boolean = ((ctx.message && ctx.message.entities) || []).some(entity => entity.type === 'bot_command');
 
   if (isCommand && ctx.chat.type !== 'private') {
     return ctx.reply(ctx.i18n.t('error.nonPrivateChat'));
