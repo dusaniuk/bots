3e9cb81f4a235026c912070449764283185e6a84
Yaroslav.Dusaniuk <yaroslav.dusaniuk@gmail.com>
2020-02-01 17:11:09 +0200

    chore(prettier): let eslint and prettier know about each other

--- test/context.mock.ts
+++ test/context.mock.ts
@@ -1,20 +1,22 @@
 import { AppContext } from '../src/shared/models/appContext';
 
-export const createMockContext = (): AppContext => (({
-  scene: {
-    state: {},
-    reenter: jest.fn(),
-    leave: jest.fn(),
-  },
-  callbackQuery: {},
-  i18n: {
-    t: jest.fn().mockImplementation((pattern) => pattern) as any,
-  },
-  reply: jest.fn().mockReturnValue(Promise.resolve({ chat: {} })),
-  replyWithMarkdown: jest.fn().mockReturnValue(Promise.resolve({})),
-  editMessageText: jest.fn().mockReturnValue(Promise.resolve({})),
-  deleteMessage: jest.fn().mockReturnValue(Promise.resolve({})),
-  telegram: {
+export const createMockContext = (): AppContext => {
+  return {
+    scene: {
+      state: {},
+      reenter: jest.fn(),
+      leave: jest.fn(),
+    },
+    callbackQuery: {},
+    i18n: {
+      t: jest.fn().mockImplementation(pattern => pattern),
+    },
+    reply: jest.fn().mockReturnValue(Promise.resolve({ chat: {} })),
+    replyWithMarkdown: jest.fn().mockReturnValue(Promise.resolve({})),
+    editMessageText: jest.fn().mockReturnValue(Promise.resolve({})),
     deleteMessage: jest.fn().mockReturnValue(Promise.resolve({})),
-  },
-} as any) as AppContext);
+    telegram: {
+      deleteMessage: jest.fn().mockReturnValue(Promise.resolve({})),
+    },
+  } as any;
+};
