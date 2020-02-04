23de5460c364a298c917e274e00843e7eb50fb3b
Yaroslav.Dusaniuk <yaroslav.dusaniuk@gmail.com>
2020-02-04 22:29:06 +0200

    TEMPORARY turn off nbr bot

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { firestore } from 'firebase-admin';
 import { Server } from './server';
 import { CONFIG } from './config';
 import { MoreBot } from './more/more.bot';
-import { NbrBot } from './nbr/nbr.bot';
+// import { NbrBot } from './nbr/nbr.bot';
 
 import { Bot } from './shared/models/bot';
 import { createDatabase } from './shared/firestore.database';
@@ -11,10 +11,10 @@ import { createDatabase } from './shared/firestore.database';
 const db: firestore.Firestore = createDatabase();
 
 const moreBot: Bot = new MoreBot(db);
-const nbrBot: Bot = new NbrBot(db);
+// const nbrBot: Bot = new NbrBot(db);
 
 if (CONFIG.environment !== 'test') {
-  nbrBot.start();
+  // nbrBot.start();
   moreBot.start();
 
   const server: Server = new Server();
