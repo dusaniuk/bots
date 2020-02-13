8c4f53f08210afe810e0ae7aa0b35d89eff46f37
Yaroslav.Dusaniuk <yaroslav.dusaniuk@gmail.com>
2020-02-13 17:57:37 +0200

    chore(logger): extend logger output

--- src/shared/logger/index.ts
+++ src/shared/logger/index.ts
@@ -3,18 +3,18 @@ import winstonLogger from './winston';
 
 export const Logger: AppLogger = {
   error: (message: string, ...meta: any[]): void => {
-    winstonLogger.error(message, meta);
+    winstonLogger.error(message, ...meta);
   },
 
   warn: (message: string, ...meta: any[]): void => {
-    winstonLogger.warn(message, meta);
+    winstonLogger.warn(message, ...meta);
   },
 
   info: (message: string, ...meta: any[]): void => {
-    winstonLogger.info(message, meta);
+    winstonLogger.info(message, ...meta);
   },
 
   debug: (message: string, ...meta: any[]): void => {
-    winstonLogger.debug(message, meta);
+    winstonLogger.debug(message, ...meta);
   },
 };
--- src/shared/logger/winston.ts
+++ src/shared/logger/winston.ts
@@ -2,7 +2,9 @@ import * as winston from 'winston';
 
 const logger: winston.Logger = winston.createLogger({
   transports: [
-    new winston.transports.Console(),
+    new winston.transports.Console({
+      format: winston.format.cli(),
+    }),
   ],
 });
 
