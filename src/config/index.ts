88f496a84ffc3961e58afbdecc36e98f4f05da5d
Yaroslav.Dusaniuk <yaroslav.dusaniuk@gmail.com>
2020-03-02 12:52:12 +0200

    feat(ci): add basic CI which will run tests and lint

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@ dotenv.config();
 export const CONFIG: AppConfig = {
   environment: process.env.ENVIRONMENT,
   isDevMode: process.env.ENVIRONMENT === 'dev',
-  port: +process.env.PORT || 8080,
+  port: +process.env.PORT ?? 8080,
   feedSchedule: {
     pattern: process.env.FEED_PATTERN,
     targetChat: +process.env.FEED_TARGET,
@@ -17,11 +17,11 @@ export const CONFIG: AppConfig = {
   },
   nbr: {
     botToken: process.env.NBR_BOT_TOKEN,
-    whitelistedChats: process.env.NBR_WHITELISTED_CHATS.split(' ').map((id: string) => +id),
+    whitelistedChats: process.env.NBR_WHITELISTED_CHATS?.split(' ').map((id: string) => +id),
   },
   firebase: {
     clientEmail: process.env.CLIENT_EMAIL,
-    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.PROJECT_ID,
   },
