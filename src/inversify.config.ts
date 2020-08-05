fd1b9e92319b805124f032b6815e830d05b2dfff
Yaroslav.Dusaniuk <yaroslav.dusaniuk@gmail.com>
2020-08-05 22:46:18 +0300

    fix(config): fix lint issue

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -3,7 +3,6 @@ import { Container } from 'inversify';
 import { moreDependencies } from './more/more.module';
 import { nbrDependencies } from './nbr/nbr.module';
 
-
 const container = new Container();
 
 container.load(
