c480f985c71e76637a5a5f634d2235ac598c3bc5
Yaroslav.Dusaniuk <yaroslav.dusaniuk@gmail.com>
2020-10-26 18:19:04 +0200

    fix(nbr): update label for chat links

--- src/nbr/telegram-bot/keyboards/chats.keyboard.ts
+++ src/nbr/telegram-bot/keyboards/chats.keyboard.ts
@@ -7,16 +7,16 @@ const CHATS_LINKS = {
   runPro: 'https://t.me/joinchat/JPBxH1jhwl1DgcBKLpGc-A',
   swim: 'https://t.me/joinchat/JPBxH1QE_EUFOZa2zwXxKg',
   climb: 'https://t.me/joinchat/FRDYHlGGoRNUbbTvy_M33w',
-  stretch: 'https://t.me/joinchat/JPBxHxEN2_t7YtZV9ph5OQ',
-  ironMan: 'https://t.me/joinchat/JPBxHw5Umu56q2iHHW-L5g',
+  ocr: 'https://t.me/NBROCR',
+  ironMan: 'https://t.me/joinchat/LxGz3k_Tew-5aTNlkhkavQ',
 };
 
 export const getChatsKeyboard = ({ i18n }: AppContext): ExtraReplyMessage => {
   return Markup.inlineKeyboard([
-    [Markup.urlButton(i18n.t('title.runPro'), CHATS_LINKS.runPro)],
+    // [Markup.urlButton(i18n.t('title.runPro'), CHATS_LINKS.runPro)],
     [Markup.urlButton(i18n.t('title.swim'), CHATS_LINKS.swim)],
     [Markup.urlButton(i18n.t('title.climb'), CHATS_LINKS.climb)],
-    [Markup.urlButton(i18n.t('title.stretch'), CHATS_LINKS.stretch)],
+    [Markup.urlButton(i18n.t('title.ocr'), CHATS_LINKS.ocr)],
     [Markup.urlButton(i18n.t('title.iron'), CHATS_LINKS.ironMan)],
   ]).extra();
 };
