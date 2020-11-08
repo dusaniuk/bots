127e3bcf3cefbe7f3d68c93fa2c50397a932e9a7
Yaroslav.Dusaniuk <yaroslav.dusaniuk@gmail.com>
2020-11-08 19:18:30 +0200

    feat(app): remove nbr bot from startup

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,11 @@ import { Bot } from './shared/interfaces';
 
 import { container } from './inversify.config';
 
-import { TYPES as NBR_TYPES } from './nbr/types';
 import { TYPES as MORE_TYPES } from './more/types';
 
 if (CONFIG.environment !== 'test') {
   const bots: Bot[] = [
     container.get<Bot>(MORE_TYPES.MORE_BOT),
-    container.get<Bot>(NBR_TYPES.NBR_BOT),
   ];
 
   bots.forEach((bot: Bot) => bot.start());
